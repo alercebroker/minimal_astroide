0d0164396ddf5c1a21baacba20e85af212c9714f
Alberto Moya Loustaunau <albertomoya87@gmail.com>
2023-05-26 00:37:57 -0400

    version 1.0.1

--- python_tests/integration/__init__.py
+++ python_tests/integration/__init__.py
@@ -4,7 +4,7 @@ SPARK = (
             SparkSession.builder.appName("IntegrationTests")
             .config(
                 "spark.jars",
-                "libs/healpix-1.0.jar,build/libs/minimal_astroide.jar",
+                "libs/healpix-1.0.jar,build/libs/minimal_astroide-1.0.1.jar",
             )
             .getOrCreate()
         )
